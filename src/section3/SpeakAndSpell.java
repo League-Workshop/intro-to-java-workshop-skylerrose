1174c9e0de40704e888abd3953c5c6c6bfc83569
League Student <league@iMac-18.attlocal.net>
2019-11-26 18:24:49 -0800

    Dragon Fight

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,26 +9,28 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+		speak("mandlebrot");
 		// 2. Catch the user's answer in a String
-
+		String answer = JOptionPane.showInputDialog("Spell the word you just heard");
+		if (answer.equalsIgnoreCase("mandlebrot")) {
+			speak("correct");
+		}
 		// 3. If the user spelled the word correctly, speak "correct"
 
 		// 4. Otherwise say "wrong"
-
+		else {
+			speak("wrong");
+		}
 		// 5. repeat the process for other words
-		
+
 	}
 
 	static void speak(String words) {
 		try {
 			Runtime.getRuntime().exec("say " + words).waitFor();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 }
-
-
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,35 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String answer = JOptionPane.showInputDialog("What year is it?");
+		// 3. Use an if statement to check if their answer is correct
+		if (answer.equalsIgnoreCase("2019")) {
+			score += 1;
+		}
+		// 4. if the user's answer was correct, add one to their score
+
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+		String answer2 = JOptionPane.showInputDialog("Is it going to snow soon in Escondido?");
+		// 6. After all the questions have been asked, print the user's score
+		if (answer2.equalsIgnoreCase("no")) {
+			score += 1;
+		}
+		String answer3 = JOptionPane.showInputDialog("What is my dog's name?");
+		if (answer3.equalsIgnoreCase("ally")) {
+			score += 1;
+			String answer4 = JOptionPane.showInputDialog("How old am I?");
+			if (answer4.equalsIgnoreCase("17")) {
+				score += 1;
+				JOptionPane.showMessageDialog(null,"You scored"+ score);
+			}
+		}
 	}
 }
